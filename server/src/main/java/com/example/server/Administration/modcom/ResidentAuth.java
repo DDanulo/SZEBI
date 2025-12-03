1dadbb1fa5a9668a87fa80b3491f7c593edad0ae
MacWalcz <maciej.walcz@gmail.com>
2025-12-03 13:19:49 +0100

    Mock comunication without autorization with Communication module

---
+++ server/src/main/java/com/example/server/Administration/modcom/ResidentAuth.java
@@ -0,0 +1,19 @@
+package com.example.server.Administration.modcom;
+
+import com.example.server.Administration.model.User;
+import com.example.server.Administration.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ResidentAuth implements IAuth {
+
+    private final UserService userService;
+
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+}
